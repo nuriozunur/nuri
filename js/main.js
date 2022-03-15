1647bdb161193bd20869a4f8a02859647495c178
nuriozunur <101643323+nuriozunur@users.noreply.github.com>
2022-03-15 15:43:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+﻿$('#ana-slider').owlCarousel({
+    slideSpeed: 2000,
+    smartSpeed: 2000,
+    items: 1,
+    autoplay: true,
+    dots: false,
+    loop: true,
+    autoplayTimeout: 5000,
+    margin: 0,
+});
+
